1ffc0465661d7572e91a97df7dd8bf37002b0219
Nupoor Verma <139352147+nuve-ra@users.noreply.github.com>
2025-05-19 23:45:31 +0530

    Update PortofolioTable.tsx

--- components/PortofolioTable.tsx
+++ components/PortofolioTable.tsx
@@ -12,25 +12,15 @@ type LiveStock = {
   
 };
 
-type CombinedStock = typeof portfolioData[0] & {
-  cmp: number | null;
-  value: number | null;
-  gainLoss: number | null;
-};
+// type CombinedStock = typeof portfolioData[0] & {
+//   cmp: number | null;
+//   value: number | null;
+//   gainLoss: number | null;
+// };
 
 //type LiveStockAPIResponse = LiveStock[];
 
 //
-//type HistoricalDataMap = {
-//  [symbol: string]: Array<{
-//    date: string;
-//    open: number;
-//    close: number;
-//    high: number;
-//    low: number;
-//    volume: number;
-//  }>;
-//};
 
 type LiveStockData = {
   [symbol: string]: {
